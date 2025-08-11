6f7edea6200822cad4b8d4fa6261a44d4d8b73f1
FGApps <74340203+FostGipa@users.noreply.github.com>
2025-08-11 09:10:03 +0300

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,40 @@ app.post('/ask', async (req, res) => {
       },
       body: JSON.stringify({
         model: "gpt-5",
+        messages: [{ role: "user", content: message }],
+        max_completion_tokens: 5000
+      })
+    });
+
+    const data = await response.json();
+
+    // ✅ Логируем весь ответ, если есть ошибка
+    if (!response.ok) {
+      console.error('⚠️ Ошибка от OpenAI API:', data);
+      return res.status(response.status).json({ error: data });
+    }
+
+    res.json({ reply: data.choices?.[0]?.message?.content || "Ошибка: пустой ответ" });
+    res.json({ reply: "Ошибка OpenAI: ${data.error?.message" });
+
+  } catch (err) {
+    console.error("❌ Ошибка сервера:", err);
+    res.status(500).json({ error: "Ошибка сервера" });
+  }
+});
+
+app.post('/ask-photo', async (req, res) => {
+  try {
+    const { message } = req.body;
+
+    const response = await fetch('https://api.openai.com/v1/chat/completions', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${OPENAI_API_KEY}`
+      },
+      body: JSON.stringify({
+        model: "gpt-5",
         messages: [
         {
           role: "user",
@@ -52,6 +86,7 @@ app.post('/ask', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`🚀 Сервер запущен на порту ${PORT}`));
+
 
 
 
